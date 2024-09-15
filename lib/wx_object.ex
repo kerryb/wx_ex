a07a9c66b4e55b48e9446309ad511634c4be5ca2
Kerry Buckley <kerryjbuckley@gmail.com>
2024-09-15 15:51:57 +0100

    Implement handle_call/3 in WxObject

--- lib/wx_object.ex
+++ lib/wx_object.ex
@@ -1,7 +1,14 @@
 defmodule WxObject do
+  @moduledoc """
+  An Elixir wrapper for Erlang’s `:wx_object` behaviour, inspired by (blatantly
+  ripped off from) `GenServer` etc.
+  """
+
+  import WxEx.Records
+
   defmacro __using__(opts) do
     quote location: :keep, bind_quoted: [opts: opts] do
-      @behaviour GenServer
+      @behaviour WxObject
       use WxEx
 
       unless Module.has_attribute?(__MODULE__, :doc) do
@@ -21,9 +28,45 @@ defmodule WxObject do
         Supervisor.child_spec(default, unquote(Macro.escape(opts)))
       end
 
-      defoverridable child_spec: 1
+      @doc false
+      def handle_call(msg, _from, state) do
+        proc =
+          case Process.info(self(), :registered_name) do
+            {_, []} -> self()
+            {_, name} -> name
+          end
+
+        # We do this to trick Dialyzer to not complain about non-local returns.
+        case :erlang.phash2(1, 1) do
+          0 ->
+            raise RuntimeError,
+                  "attempted to call WxObject #{inspect(proc)} but no handle_call/3 clause was provided"
+
+          1 ->
+            {:stop, {:bad_call, msg}, state}
+        end
+      end
+
+      defoverridable child_spec: 1, handle_call: 3
     end
   end
+
+  @callback init(args :: term()) ::
+              {record(:wx_ref, ref: term(), type: term(), state: term()), state: term()}
+              | {record(:wx_ref, ref: term(), type: term(), state: term()), state :: term(),
+                 timeout() | :hibernate}
+              | {:stop, reason :: term()}
+              | :ignore
+
+  @callback handle_call(request :: term(), from :: GenServer.server(), state :: term()) ::
+              {:reply, reply, new_state}
+              | {:reply, reply, new_state,
+                 timeout | :hibernate | {:continue, continue_arg :: term()}}
+              | {:noreply, new_state}
+              | {:noreply, new_state, timeout | :hibernate | {:continue, continue_arg :: term()}}
+              | {:stop, reason, reply, new_state}
+              | {:stop, reason, new_state}
+            when reply: term(), new_state: term(), reason: term()
 
   def start_link(module, args, options \\ []) do
     case options[:name] do
@@ -32,6 +75,55 @@ defmodule WxObject do
 
       name when is_atom(name) ->
         :wx_object.start_link({:local, name}, module, args, Keyword.delete(options, :name))
+    end
+  end
+
+  @doc """
+  Makes a synchronous call to the `server` and waits for its reply.
+
+  The client sends the given `request` to the server and waits until a reply
+  arrives or a timeout occurs. `c:handle_call/3` will be called on the server
+  to handle the request.
+
+  `server` can be any of the values described in the "Name registration"
+  section of the documentation for this module.
+
+  ## Timeouts
+
+  `timeout` is an integer greater than zero which specifies how many
+  milliseconds to wait for a reply, or the atom `:infinity` to wait
+  indefinitely. The default value is `5000`. If no reply is received within
+  the specified time, the function call fails and the caller exits. If the
+  caller catches the failure and continues running, and the server is just late
+  with the reply, it may arrive at any time later into the caller's message
+  queue. The caller must in this case be prepared for this and discard any such
+  garbage messages that are two-element tuples with a reference as the first
+  element.
+  """
+  @spec call(GenServer.server(), term(), timeout()) :: term
+  def call(server, request, timeout \\ 5000)
+      when (is_integer(timeout) and timeout >= 0) or timeout == :infinity do
+    case GenServer.whereis(server) do
+      nil ->
+        exit({:noproc, {__MODULE__, :call, [server, request, timeout]}})
+
+      pid ->
+        try do
+          :wx_object.call(pid, request, timeout)
+        rescue
+          e ->
+            case e do
+              %{original: {{%RuntimeError{} = error, stacktrace}, _call}} ->
+                reraise error, stacktrace
+
+              _ ->
+                reraise e, __STACKTRACE__
+            end
+        catch
+          :exit, reason ->
+            exit({reason, {__MODULE__, :call, [server, request, timeout]}})
+          {:ok, res} -> res
+        end
     end
   end
 
--- test/wx_object_test.exs
+++ test/wx_object_test.exs
@@ -1,7 +1,8 @@
 defmodule WxObjectTest do
   use ExUnit.Case, async: true
+  import ExUnit.CaptureLog
 
-  defmodule AnonymousWxObject do
+  defmodule BasicWxObject do
     use WxObject
 
     def start_link(args) do
@@ -16,11 +17,11 @@ defmodule WxObjectTest do
     end
   end
 
-  defmodule NamedWxObject do
+  defmodule FullWxObject do
     use WxObject
 
     def start_link(args) do
-      wx_ref() = ref = WxObject.start_link(__MODULE__, args, name: NamedWxObject)
+      wx_ref() = ref = WxObject.start_link(__MODULE__, args, name: __MODULE__)
       {:ok, WxObject.get_pid(ref)}
     end
 
@@ -29,17 +30,36 @@ defmodule WxObjectTest do
       frame = :wxFrame.new(:wx.null(), 1, "")
       {frame, nil}
     end
+
+    def handle_call(:ping, _from, state) do
+      {:reply, :pong, state}
+    end
   end
 
   describe "WxObject" do
     test "Wraps :wx_object.start_link/3, and generates a default child_spec/1 implementation" do
-      {:ok, pid} = start_supervised(AnonymousWxObject)
+      {:ok, pid} = start_supervised(BasicWxObject)
       assert is_pid(pid)
     end
 
     test "Allows specification of a server name" do
-      start_supervised!(NamedWxObject)
-      assert is_pid(GenServer.whereis(NamedWxObject))
+      start_supervised!(FullWxObject)
+      assert is_pid(GenServer.whereis(FullWxObject))
+    end
+
+    test "forwards calls" do
+      {:ok, pid} = start_supervised(FullWxObject)
+      assert WxObject.call(pid, :ping) == :pong
+    end
+
+    test "raises a helpful error on call when handle_call/3 is not implemented" do
+      {:ok, pid} = start_supervised(BasicWxObject)
+
+      assert_raise(
+        RuntimeError,
+        "attempted to call WxObject #{inspect(pid)} but no handle_call/3 clause was provided",
+        fn -> capture_log(fn -> WxObject.call(pid, :ping) end) end
+      )
     end
   end
 end
