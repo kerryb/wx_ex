a2e26cb0f0e35a1ca400586053dd8a114f179aa7
Kerry Buckley <kerryjbuckley@gmail.com>
2024-09-20 12:25:04 +0100

    Just “use WxEx” in test

--- test/wx_ex_test.exs
+++ test/wx_ex_test.exs
@@ -1,17 +1,16 @@
 defmodule WxExTest do
   use ExUnit.Case, async: true
+  use WxEx
 
   import Record
 
-  require WxEx.Records
-
   describe "WxEx" do
     test "Generates wrappers for :wx constant macros" do
-      assert WxEx.Constants.WxWidgets.wxALL() == 240
+      assert wxALL() == 240
     end
 
     test "Generates wrappers for :gl constant macros" do
-      assert WxEx.Constants.OpenGL.gl_POINT() == 6912
+      assert gl_POINT() == 6912
     end
 
     test "Generates wrappers for records in include files" do
@@ -20,13 +19,6 @@ defmodule WxExTest do
 
     test "Generates wrappers for records in src files" do
       assert is_record(WxEx.Records.wx_ref())
-    end
-
-    test "Imports all constants and macros with `use WxEx`" do
-      use WxEx
-
-      assert wxALL() == 240
-      assert is_record(wx())
     end
   end
 end
